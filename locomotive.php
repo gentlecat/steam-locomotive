3667a0fd5715b0b6370178b6ff4475780eb780fd
Roman Tsukanov <roman@tsukanov.me>
2013-04-07 06:36:16 +1000

    Removed dynamic definition of API interfaces and tools.

--- locomotive.php
+++ locomotive.php
@@ -1,13 +1,10 @@
 <?php
 
-use Locomotive\Tools;
-
 define('LOCOMOTIVE_PATH', dirname(__FILE__) . '/');
 define('LOCOMOTIVE_CORE_PATH', LOCOMOTIVE_PATH . 'core/');
 
 require LOCOMOTIVE_CORE_PATH . 'exceptions.php';
 
-//use Locomotive\Tools;
 // Importing interfaces
 require LOCOMOTIVE_CORE_PATH . 'web_interface.php';
 define('LOCOMOTIVE_INTERFACES_PATH', LOCOMOTIVE_CORE_PATH . 'interfaces/');
@@ -36,12 +33,19 @@ class Locomotive
         $GLOBALS['api_key'] = $api_key;
 
         // Defining interfaces
-        foreach (glob(LOCOMOTIVE_INTERFACES_PATH . '*.php') as $filename) {
-            $filename = str_replace(LOCOMOTIVE_INTERFACES_PATH, '', $filename);
-            $interface_name = substr($filename, 0, -4);
-            $class = '\\Locomotive\\WebInterfaces\\' . $interface_name;
-            $this->$interface_name = new $class();
-        }
+        $this->IDOTA2Match_570 = new \Locomotive\WebInterfaces\IDOTA2Match_570();
+        $this->IDOTA2Match_816 = new \Locomotive\WebInterfaces\IDOTA2Match_816();
+        $this->IDOTA2Match_205790 = new \Locomotive\WebInterfaces\IDOTA2Match_205790();
+        $this->IEconDOTA2_570 = new \Locomotive\WebInterfaces\IEconDOTA2_570();
+        $this->IEconDOTA2_816 = new \Locomotive\WebInterfaces\IEconDOTA2_816();
+        $this->IEconDOTA2_205790 = new \Locomotive\WebInterfaces\IEconDOTA2_205790();
+        $this->IPlayerService = new \Locomotive\WebInterfaces\IPlayerService();
+        $this->ISteamApps = new \Locomotive\WebInterfaces\ISteamApps();
+        $this->ISteamGameServerAccount = new \Locomotive\WebInterfaces\ISteamGameServerAccount();
+        $this->ISteamRemoteStorage = new \Locomotive\WebInterfaces\ISteamRemoteStorage();
+        $this->ISteamUser = new \Locomotive\WebInterfaces\ISteamUser();
+        $this->ISteamUserStats = new \Locomotive\WebInterfaces\ISteamUserStats();
+        $this->ISteamWebAPIUtil = new \Locomotive\WebInterfaces\ISteamWebAPIUtil();
 
         $this->tools = new LocomotiveTools();
     }
@@ -54,12 +58,10 @@ class LocomotiveTools
     public function __construct()
     {
         // Defining tools
-        foreach (glob(LOCOMOTIVE_TOOLS_PATH . '*.php') as $filename) {
-            $filename = str_replace(LOCOMOTIVE_TOOLS_PATH, '', $filename);
-            $tool_name = substr($filename, 0, -4);
-            $class = '\\Locomotive\\Tools\\' . $tool_name;
-            $this->$tool_name = new $class();
-        }
+        $this->app = new \Locomotive\Tools\App();
+        $this->dota = new \Locomotive\Tools\Dota();
+        $this->group = new \Locomotive\Tools\Group();
+        $this->user = new \Locomotive\Tools\User();
     }
 
 }
