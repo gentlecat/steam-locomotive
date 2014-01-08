da1d7c6177d21883e833fcaf78020f2afebf6308
Roman Tsukanov <roman@tsukanov.me>
2014-01-08 15:11:19 +1000

    Added some useful functions to dota tools.

--- core/tools/dota.php
+++ core/tools/dota.php
@@ -4,14 +4,40 @@ namespace Locomotive\Tools;
 class Dota extends Tool
 {
 
+
+    function getHeroPickerData()
+    {
+        return json_decode(parent::getContent("http://www.dota2.com/jsfeed/heropickerdata"));
+    }
+
     function getHeroIconURL($hero_name)
     {
         return 'http://media.steampowered.com/apps/dota2/images/heroes/' . $hero_name . '_sb.png';
     }
 
+    function getItemData()
+    {
+        return json_decode(parent::getContent("http://www.dota2.com/jsfeed/itemdata"));
+    }
+
     function getItemIconURL($item_name)
     {
         return 'http://media.steampowered.com/apps/dota2/images/items/' . $item_name . '_lg.png';
+    }
+
+    function getAbilityData()
+    {
+        return json_decode(parent::getContent("http://www.dota2.com/jsfeed/abilitydata"));
+    }
+
+    function getUniqueUsers()
+    {
+        return json_decode(parent::getContent("http://www.dota2.com/jsfeed/uniqueusers"));
+    }
+
+    function getIntlPrizePool()
+    {
+        return json_decode(parent::getContent("http://www.dota2.com/jsfeed/intlprizepool"));
     }
 
 }
