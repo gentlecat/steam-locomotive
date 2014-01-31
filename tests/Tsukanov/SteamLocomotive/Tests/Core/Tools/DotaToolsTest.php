0e04980d77913154e001cc9e6809dcb46a82b192
Roman Tsukanov <roman@tsukanov.me>
2014-01-31 21:56:31 +1000

    Added more tests for dota tools. Related to #8.

--- tests/Tsukanov/SteamLocomotive/Tests/Core/Tools/DotaToolsTest.php
+++ tests/Tsukanov/SteamLocomotive/Tests/Core/Tools/DotaToolsTest.php
@@ -8,20 +8,46 @@ class DotaToolsTest extends \PHPUnit_Framework_TestCase
 
     public function testGetHeroIconURL()
     {
-        $hero_name = 'techies';
-        $expected = 'http://media.steampowered.com/apps/dota2/images/heroes/' . $hero_name . '_sb.png';
         $dota_tools = new Dota();
-        $actual = $dota_tools->getHeroIconURL($hero_name);
-        $this->assertEquals($expected, $actual);
+        $hero_name = 'techies';
+        $this->assertEquals('http://media.steampowered.com/apps/dota2/images/heroes/' . $hero_name . '_sb.png', $dota_tools->getHeroIconURL($hero_name));
     }
 
     public function testGetItemIconURL()
     {
-        $item_name = 'dagon';
-        $expected = 'http://media.steampowered.com/apps/dota2/images/items/' . $item_name . '_lg.png';
         $dota_tools = new Dota();
-        $actual = $dota_tools->getItemIconURL($item_name);
-        $this->assertEquals($expected, $actual);
+        $item_name = 'dagon';
+        $this->assertEquals('http://media.steampowered.com/apps/dota2/images/items/' . $item_name . '_lg.png', $dota_tools->getItemIconURL($item_name));
+    }
+
+    function testGetHeroPickerData()
+    {
+        $dota_tools = new Dota();
+        $dota_tools->getHeroPickerData();
+    }
+
+    function testGetItemData()
+    {
+        $dota_tools = new Dota();
+        $dota_tools->getItemData();
+    }
+
+    function testGetAbilityData()
+    {
+        $dota_tools = new Dota();
+        $dota_tools->getAbilityData();
+    }
+
+    function testGetUniqueUsers()
+    {
+        $dota_tools = new Dota();
+        $dota_tools->getUniqueUsers();
+    }
+
+    function testGetIntlPrizePool()
+    {
+        $dota_tools = new Dota();
+        $dota_tools->getIntlPrizePool();
     }
 
 }
