ac948e91ff48ffe537733fe18b9dae78b66f3332
Roman Tsukanov <roman@tsukanov.me>
2014-01-31 21:56:59 +1000

    Added more tests for store tools. Related to #8.

--- tests/Tsukanov/SteamLocomotive/Tests/Core/Tools/StoreToolsTest.php
+++ tests/Tsukanov/SteamLocomotive/Tests/Core/Tools/StoreToolsTest.php
@@ -5,14 +5,54 @@ use Tsukanov\SteamLocomotive\Core\Tools\Store;
 
 class StoreToolsTest extends \PHPUnit_Framework_TestCase
 {
+    protected $appids = array(10, 50);
+    protected $packageids = array(4116);
 
     public function testGetAppLogoURL()
     {
-        $app_id = 10;
-        $expected = 'http://cdn.steampowered.com/v/gfx/apps/' . $app_id . '/header.jpg';
         $store_tools = new Store();
-        $actual = $store_tools->getAppLogoURL($app_id);
-        $this->assertEquals($expected, $actual);
+
+        $app_id = $this->appids[0];
+        $this->assertEquals('http://cdn.steampowered.com/v/gfx/apps/' . $app_id . '/header.jpg', $store_tools->getAppLogoURL($app_id));
+
+        $app_id = $this->appids[1];
+        $this->assertEquals('http://cdn.steampowered.com/v/gfx/apps/' . $app_id . '/header.jpg', $store_tools->getAppLogoURL($app_id));
+    }
+
+    function testGetAppDetails()
+    {
+        $store_tools = new Store();
+        $store_tools->getAppDetails();
+    }
+
+    function testGetAppUserDetails()
+    {
+        $store_tools = new Store();
+        $store_tools->getAppUserDetails($this->appids);
+    }
+
+    function testGetPackageDetails()
+    {
+        $store_tools = new Store();
+        $store_tools->getPackageDetails($this->packageids);
+    }
+
+    function testGetFeatured()
+    {
+        $store_tools = new Store();
+        $store_tools->getFeatured();
+    }
+
+    function testGetFeaturedCategories()
+    {
+        $store_tools = new Store();
+        $store_tools->getFeaturedCategories();
+    }
+
+    function testGetSalePage()
+    {
+        $store_tools = new Store();
+        $store_tools->getSalePage(0);
     }
 
 }
